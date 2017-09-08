0e2eb98dc62b3ec5df700d24238db482de89f0d4
David Gailey <dave.g.github@gmail.com>
2017-09-08 16:08:41 -0400

    additional setup for simple restful api

--- server.js
+++ server.js
@@ -1,31 +1,55 @@
 var express = require('express'),
 	morgan = require('morgan'),
-	async = require('async')''
 	app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+// use morgan for logging
 app.use(morgan('dev'));
+
+// serve static content, express will use index.html for the "/" route
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
+// comment CRUD api
+	// Create comments
+	app.put('/api/comments', function(request, response) {
+		
+	});
 
-app.get('/', function(request, response) {
-	response.render('pages/index');
-});
+	// Read comments
+	app.get('/api/comments', function(request, response) {
+		var commentList = [
+			{id:1, author: 'Jerry', body: 'I like cheese'},
+			{id:2, author: 'Tom', body: 'I like mice!'}
+		]; // to do: query mongoDB database
 
-app.get('/api/comments', function(request, response) {
-	var commentList = [
-		{id:1, author: 'Jerry', body: 'I like cheese'},
-		{id:2, author: 'Tom', body: 'I like mice!'}
-	]; 
-	response.send(commentList);
-});
+		sendSuccessResponse(response,commentList);
+	});
+
+	// Update comments
+	app.post('/api/comments', function(request, response) {
+		
+	});
+
+	// Delete comments
+	app.delete('/api/comments', function(request, response) {
+		
+	});
+
+function sendSuccessResponse(response,obj){
+	if(arguments.length !== 2){
+		console.error('sendSuccessResponse needs 2 arguments');
+		throw new Error();
+	}
+
+	response.setHeader('Content-Type','application/json');
+	response.status(200)
+		.send(JSON.stringify(obj))
+		.end();
+}
 
 app.listen(app.get('port'), function() {
-	console.log('Node app is running on port', app.get('port'));
+	console.log('Node app is running on port ', app.get('port'));
 });
 
 
